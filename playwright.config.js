c874404d0b18c9a3459ab6a85ce7e87e996aa2a2
Zoia Stadnyk <stadnikzoa@gmail.com>
2024-10-23 19:59:32 +0300

    tests and READMY.md

--- playwright.config.js
+++ playwright.config.js
@@ -29,6 +29,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false
   },
 
   /* Configure projects for major browsers */
